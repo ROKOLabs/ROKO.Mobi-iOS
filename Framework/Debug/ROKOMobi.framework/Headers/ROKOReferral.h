1819cb2df5a518658de51a1505bb524e6d9a7cff
Alexey Golovenkov <alexey@rokolabs.com>
2015-12-29 17:48:17 +0300

    Version 0.4.4

--- Framework/Debug/ROKOMobi.framework/Headers/ROKOReferral.h
+++ Framework/Debug/ROKOMobi.framework/Headers/ROKOReferral.h
@@ -8,10 +8,30 @@
 
 #import "ROKOComponent.h"
 #import "ROKOReferralDiscountItem.h"
+#import "ROKOReferralCampaignInfo.h"
+
+typedef void (^ROKOReferralDiscountInfoResponseBlock)(ROKOReferralCampaignInfo *campaignInfo, NSError *error);
+typedef void (^ROKOReferralActivateDiscountResponseBlock)(NSNumber *discountId, NSError *error);
+typedef void (^ROKOReferralCompleteDiscountResponseBlock)(NSNumber *discountId, NSNumber *referrerUserId, NSError *error);
 
 @interface ROKOReferral : ROKOComponent
 
 - (void)loadReferralDiscountsList:(ROKOItemsListCompletionBlock)completionBlock;
 - (void)markReferralDiscountAsUsed:(NSNumber *)discountId completionBlock:(ROKOMarkDiscountCompletionBlock)completionBlock;
+
+/**
+ *  Loads referral campaing info
+ */
+- (void)loadDiscountInfoWithCode:(NSString *)code completionBlock:(ROKOReferralDiscountInfoResponseBlock)completionBlock;
+
+/**
+ *  Activates discount by the given referral code
+ */
+- (void)activateDiscountWithCode:(NSString *)code completionBlock:(ROKOReferralActivateDiscountResponseBlock)completionBlock;
+
+/**
+ *  Completes discount with given referral code
+ */
+- (void)completeDiscountWithCode:(NSString *)code completionBlock:(ROKOReferralCompleteDiscountResponseBlock)completionBlock;
 
 @end
